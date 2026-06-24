68c40b105c813288afae3f557ba37e49f92b3156
niels <353468@viauc.dk>
2026-06-25 00:17:09 +0200

    Add babel-plugin-react-compiler to dependencies and update base path in Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import babel from "@rolldown/plugin-babel";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "TrackrV2",
+  base: "/TrackrV2/",
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
 });
