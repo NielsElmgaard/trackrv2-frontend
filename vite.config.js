b3f5ca686f67ffb1d719e56a5865463cba0b6bbb
niels <353468@viauc.dk>
2026-06-25 12:06:44 +0200

    Refactor Vite config to support dynamic base path and improve server proxy settings

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,22 @@ import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: "/TrackrV2/",
-  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === "production" ? "/TrackrV2/" : "/", // '/' for local dev
+
+    plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+
+    server: {
+      proxy: {
+        // Points local 'http://localhost:5173/api' requests to local C# backend
+        // Only used when running backend locally at port 5001
+        "/api": {
+          target: "http://localhost:5001",
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+  };
 });
